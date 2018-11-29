152f48490dfcf6348cae9cc047e8c0a970e53ef8
davidKolesar <djkolesa@gmail.com>
2018-11-29 12:03:03 -0500

    David Kolesar has added logging, multithreading, disconnect functionality, username validation, and added documentation

--- HillaryChatServer/src/main/java/com/mycompany/server/ChatServer.java
+++ HillaryChatServer/src/main/java/com/mycompany/server/ChatServer.java
@@ -2,10 +2,12 @@ package com.mycompany.server;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.HashSet;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
@@ -66,14 +68,101 @@ public class ChatServer {
 		}
 	}
 
+	/**
+	 * UserInteractionService deals with each unique user's connection, name, and
+	 * messages
+	 *
+	 */
 	private static class UserInteractionService extends Thread {
 		private String userName;
 		private BufferedReader reader;
 		private PrintWriter writer;
 		private Socket socket;
 
+		/**
+		 * Boilerplate to handle threading by assigning socket established by listener
+		 * via PORT
+		 */
 		public UserInteractionService(Socket socket) {
 			this.socket = socket;
+		}
+
+		/*
+		 * (non-Javadoc)
+		 * 
+		 * @see java.lang.Thread#run() Checks for unique username
+		 */
+		public void run() {
+			boolean userNameChallengeSatisfied = false;
+			try {
+				LOGGER.info("New client is attempting to connect");
+				// Creates BufferedReader to display text through the socket to users.
+				reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				writer = new PrintWriter(socket.getOutputStream(), true);
+
+				/*
+				 * Requests username from user and challenges them if it already exists (or is
+				 * null / empty)
+				 */
+				if (userNameChallengeSatisfied == false) {
+					LOGGER.log(Level.FINE, "Sending unique name request to client");
+					writer.println(SUBMIT_NAME_REQUEST);
+					userName = reader.readLine();
+					if (userName == null || userName.isEmpty()) {
+						LOGGER.log(Level.FINE, "Client returned a username that is empty or null.");
+					} else {
+						/*
+						 * Locks HashSet of names to enforce that only one thread at a time should be
+						 * able to access the method in order to avoid racing condition (duplicate
+						 * usernames)
+						 */
+						synchronized (userNames) {
+							if (!userNames.contains(userName)) {
+								LOGGER.log(Level.FINE, "Client returned unique username");
+								userNames.add(userName);
+								userNameChallengeSatisfied = true;
+							}
+						}
+					}
+				}
+
+				// Add user's unique printWriter to the HashSet
+				writer.println(RECEIVE_NAME_CONFIRMATION);
+				printWriters.add(writer);
+
+				// Display messages from all users
+				while (true) {
+					String input = reader.readLine();
+					if (input == null) {
+						// User cannot send a null message
+						return;
+					}
+					for (PrintWriter writer : printWriters) {
+						writer.println(SUBMIT_MESSAGE_PREFIX + " " + userName + ":" + input);
+					}
+				}
+			} catch (IOException e) {
+				LOGGER.warning(e.getMessage());
+			}
+
+			/*
+			 * If a user exits the application, remove their username and printWriter from
+			 * their corresponding HashSets and close the user's old socket.
+			 */
+			finally {
+				LOGGER.log(Level.FINE, "Client disconnected from Hillary");
+				if (userName != null) {
+					userNames.remove(userName);
+				}
+				if (writer != null) {
+					printWriters.remove(writer);
+				}
+				try {
+					socket.close();
+				} catch (IOException e) {
+					LOGGER.warning(e.getMessage());
+				}
+			}
 		}
 	}
 }
